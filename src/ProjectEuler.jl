540fdc62dc7e6e8d509afe9285850457e29ccb32
Ondrej Winter <ondrej.winter@gmail.com>
2024-06-23 12:47:13 +0200

    feat: problem 6

--- src/ProjectEuler.jl
+++ src/ProjectEuler.jl
@@ -5,8 +5,9 @@ include("problems/p_0002.jl")
 include("problems/p_0003.jl")
 include("problems/p_0004.jl")
 include("problems/p_0005.jl")
+include("problems/p_0006.jl")
 
-solved_problems = [p_0001, p_0002, p_0003, p_0004, p_0005]
+solved_problems = [p_0001, p_0002, p_0003, p_0004, p_0005, p_0006]
 
 export solved_problems
 
---
+++ src/problems/p_0006.jl
@@ -0,0 +1,14 @@
+function sum_of_squares(n::Integer)
+    return n * (n + 1) * (2 * n + 1) ÷ 6
+end
+
+function square_of_sum(n::Integer)
+    sum = n * (n + 1) ÷ 2
+    return sum * sum
+end
+
+function p_0006(n::Integer=100)
+    sum_squares = sum_of_squares(n)
+    square_sum = square_of_sum(n)
+    return square_sum - sum_squares
+end
