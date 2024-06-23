7572342580607609b8829805e141e9d6a728f3b5
Ondrej Winter <ondrej.winter@gmail.com>
2024-06-23 13:11:45 +0200

    feat: problem 9

---
+++ src/problems/p_0009.jl
@@ -0,0 +1,13 @@
+function p_0009(sum::Integer=1000)
+    for m in 2:floor(Int, sqrt(sum))
+        for n in 1:(m - 1)
+            a = m^2 - n^2
+            b = 2 * m * n
+            c = m^2 + n^2
+            if a + b + c == sum
+                return a * b * c
+            end
+        end
+    end
+    return nothing
+end
