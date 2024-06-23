311f442cfedbccaab9ce9017a29493179bfa978c
Ondrej Winter <ondrej.winter@gmail.com>
2024-06-23 13:01:57 +0200

    feat: problem 8

--- src/ProjectEuler.jl
+++ src/ProjectEuler.jl
@@ -7,8 +7,9 @@ include("problems/p_0004.jl")
 include("problems/p_0005.jl")
 include("problems/p_0006.jl")
 include("problems/p_0007.jl")
+include("problems/p_0008.jl")
 
-solved_problems = [p_0001, p_0002, p_0003, p_0004, p_0005, p_0006, p_0007]
+solved_problems = [p_0001, p_0002, p_0003, p_0004, p_0005, p_0006, p_0007, p_0008]
 
 export solved_problems
 
